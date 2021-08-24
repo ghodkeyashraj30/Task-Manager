45dadcc55a27f7bb6f5225f29371dff997f7bd04
Yashraj Ghodke <78252070+ghodkeyashraj30@users.noreply.github.com>
2021-08-24 11:29:50 +0530

    Add files via upload

---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,41 @@
+import { TaskService } from './../../task.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { List } from 'src/app/models/list.model';
+import { Task } from 'src/app/models/task.model';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+
+  lists:List[];
+  tasks:Task[];
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params:Params)=>{
+      this.taskService.getTasks(params.listId).subscribe((tasks:Task[])=>{
+       this.tasks = tasks;
+      })
+    })
+
+    this.taskService.getLists().subscribe((lists:List[]) => {
+      this.lists = lists;
+    })
+  }
+
+  onTaskClick(task: Task){
+    //we want to set the task to completed
+    this.taskService.complete(task).subscribe(()=>{
+      //the task has been complete to successfully
+      console.log("Completed Successfully");
+      task.completed = !task.completed;
+    })
+
+  }
+
+}
